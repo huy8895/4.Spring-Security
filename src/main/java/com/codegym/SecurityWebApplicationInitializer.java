7a32c82a36faab95558680557656245b8abaa91b
huy8895 <huy8895@gmail.com>
2020-09-30 11:16:56 +0700

    Đăng ký springSecurityFilterChain

---
+++ src/main/java/com/codegym/SecurityWebApplicationInitializer.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Cấu hình này chỉ đăng ký bộ lọc springSecurityFilterChain
+ * cho mỗi URL cho ứng dụng của bạn.
+ */
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
